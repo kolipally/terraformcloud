149d95fec499799a1d433c552f980422eee13b0b
root <root@instance-2.us-west4-b.c.certain-analyst-330409.internal>
2021-11-10 17:14:42 +0000

    frst file

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "google" {
+  project = "var.project"
+  region  = "us-central1"
+}
+variable "machine" {
+  type    = string
+  default = "f1-micro"
+}
+variable "project" {
+  type = string
+}
+resource "google_compute_instance" "vm" {
+  name         = "instance1"
+  machine_type = "var.machine"
+  zone         = "us-central-1a"
+  network_interface {
+    network = "default"
+  }
+  boot_disk {
+    initialize_params {
+      image = "rhel-7-v20210916"
+    }
+  }
+}
